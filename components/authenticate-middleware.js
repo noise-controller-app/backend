53949ca53d4b1bd160a29f171dde058e7c8f9628
thejhubbs <thejhubbs@gmail.com>
2019-09-25 19:59:23 -0400

    Reseed

--- components/authenticate-middleware.js
+++ components/authenticate-middleware.js
@@ -5,29 +5,24 @@
 
 
 
-  // const user = req.session.user
-  // console.log("checking", req.session.user, "USER", user, req.session)
-  // if(user) {
-  //   next();
-  // } else {
-  //   res.status(400).json({message: "Please log in."})
-  // }
+
 
 function restricted(req, res, next) {
-  next();
+  const user = req.session.user
+  if(user) {
+    next();
+  } else {
+    res.status(400).json({message: "Please log in."})
+  }
 }
 
 function restricted_by_profile(req, res, next) {
-  next();
+  const user = req.session.user
+  if(user && user.teacher_id == req.params.id) {
+    next();
+  } else {
+    res.status(400).json({message: "Please log in."})
+  }
 }
-
-// const user = req.session.user
-// console.log("checking", req.session.user, user, req.session)
-// if(user && user.teacher_id == req.params.id) {
-//   next();
-// } else {
-//   res.status(400).json({message: "Please log in."})
-// }
-
 
 module.exports = {restricted, restricted_by_profile}
